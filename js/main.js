7ed96a64c8c5573232acb53663f3db591ba7e128
Shaun <shaun@meshfresh.com>
2017-06-01 10:45:52 -0400

    Merge frontend/backend dev

---
+++ js/main.js
@@ -0,0 +1,129 @@
+// jQuery(document).ready(function($){
+//
+//   //Are we loaded?
+//   console.log('New theme loaded!');
+//
+//   //Let's do something awesome!
+//
+// });
+
+var searchClose = document.getElementById('searchModalClose'),
+   listingInterface = document.getElementById('listingInterface'),
+   altSearchClose1 = document.getElementById('landingBrowsePrograms'),
+   searchModal = document.getElementById('searchModal'),
+   modalNonFilter = document.getElementById('modalNonFilter'),
+   nonFilterContent = document.getElementById('nonFilterContent'),
+   modalFilterContent = document.getElementById('modalFilterContent'),
+   filterContainer = document.getElementById('filterContainer'),
+   selectionView = document.getElementById('selectionView'),
+   detailPane = document.getElementById('detailPane'),
+   detailNavTop = document.getElementById('detailNavTop'),
+   detailNavBottom = document.getElementById('detailNavBottom'),
+   detailPaneContent = document.getElementById('detailPaneContent'),
+   cancelFilters = document.getElementById('cancelFilters'),
+   filterBottomNav = document.getElementById('filterBottomNav'),
+   filterView = new TimelineMax(),
+   landingView = new TimelineMax(),
+   detailView = new TimelineMax();
+
+//Langing view close animation
+landingView.paused(true)
+   .add("open")
+   .to(searchModal, 0.75, {css:{top:"-100%", autoAlpha:0}, ease: Elastic.easeIn.config(1, 1)}, "open")
+   .to(listingInterface, 0.75, {css:{left:"0"}, ease: Power3.easeInOut, delay:0.4}, "open")
+   .add("closed");
+
+//Landing view close function
+var landingViewClose = function(event){
+   event.preventDefault();
+   landingView.play();
+};
+
+//Play landing view close
+searchClose.addEventListener("click", landingViewClose);
+
+altSearchClose1.addEventListener("click", landingViewClose);
+
+//Filter view
+var filterTriggers = document.getElementsByClassName('dropdown-trigger');
+var active = '';
+
+//Filter view transition definition
+filterView.paused(true)
+   .add("closed")
+   .to(nonFilterContent, 0.2, {css:{autoAlpha:0}}, "closed")
+   .to(filterContainer, 0.2, {css:{top:"240px", autoAlpha:1}}, "open")
+   .to(filterBottomNav, 0.2, {css:{autoAlpha:1}}, "open")
+   .to('.block-interior', 0.1, {className:'+=filter-view'}, "open")
+   .add("open");
+
+var openFilters = function(event){
+   active = event.target;
+   //Reset filter trigger functions
+   for (var i = 0; i < filterTriggers.length; i++) {
+      filterTriggers[i].className = 'dropdown-trigger';
+      filterTriggers[i].onclick = openFilters;
+      filterContainer.className = 'filters';
+   }
+   //Choose which filters to open
+   filterContainer.className += (' ' + active.id);
+   //Switch from listing to filter view
+   filterView.play();
+   //Highlight active filter category
+   active.className = "dropdown-trigger active-filter";
+   //Allow active to close filters
+   active.onclick = closeFilters;
+};
+
+var closeFilters = function(event){
+   //Remove category highlight & filter container contents
+   active.className = "dropdown-trigger";
+   //Reset filter container classes
+   filterContainer.className = "filters";
+   //Reveal listing view
+   filterView.reverse();
+   //Revert onclick
+   event.target.onclick = openFilters;
+};
+
+for (var i = 0; i < filterTriggers.length; i++) {
+   filterTriggers[i].onclick = openFilters;
+};
+
+cancelFilters.onclick = function(){
+   //Reset active filter category styles and functions
+   active.className = "dropdown-trigger";
+   active.onclick = openFilters;
+   active = '';
+   //Reset filter container classes
+   filterContainer.className = "filters";
+   //Reveal listing view
+   filterView.reverse();
+};
+
+//Detail View
+detailView.paused(true)
+   .add("closed")
+   .to(selectionView, 0.2, {css:{autoAlpha:0}}, "closed")
+   .to(detailPane, 0.2, {css:{autoAlpha:1}}, "closed")
+   .to(detailNavTop, 0.2, {css:{autoAlpha:1, top:"80px"}}, "open")
+   .to(detailNavBottom, 0.2, {css:{autoAlpha:1, bottom:"0"}}, "open")
+   .to(detailPaneContent, 0.2, {css:{autoAlpha:1, left:"0"}}, "open")
+   .add("open");
+
+var programBlocks = document.getElementsByClassName('indiv-block');
+var openDetails = function(event){
+   event.preventDefault();
+   detailView.play();
+   selectionView.className += 'detail-view';
+};
+
+var closeDetails = function(){
+   detailView.reverse();
+   selectionView.className = '';
+}
+
+for (var i = 0; i < programBlocks.length; i++) {
+   programBlocks[i].onclick = openDetails;
+}
+detailNavTop.onclick = closeDetails;
