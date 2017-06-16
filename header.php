e4fd12740c382b1d67c8cb77ffcf4ac37bcdb333
Shaun <shaun@meshfresh.com>
2017-06-16 10:45:56 -0400

    Add bugherd to head

--- header.php
+++ header.php
@@ -34,6 +34,17 @@
 	================================================== -->
 	<link href="https://fonts.googleapis.com/css?family=Cabin" rel="stylesheet">
 
+	<!-- Bugherd
+	================================================== -->
+	<script type='text/javascript'>
+	(function (d, t) {
+	  var bh = d.createElement(t), s = d.getElementsByTagName(t)[0];
+	  bh.type = 'text/javascript';
+	  bh.src = 'https://www.bugherd.com/sidebarv2.js?apikey=yqsy4bao6vlyvoxgc15tbq';
+	  s.parentNode.insertBefore(bh, s);
+	  })(document, 'script');
+	</script>
+
 	<?php wp_head(); ?>
 
 </head>
